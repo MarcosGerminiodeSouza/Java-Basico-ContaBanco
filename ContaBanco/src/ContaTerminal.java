a33cb88d5c9a35c3eb071ccf27088f5c88f4d3f5
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2024-03-05 12:13:48 -0300

    Commit Inicial

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,8 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        //TODO: Inportar a classe Scanner.
+        
+        //TODO: Obter os valores digitados no terminal.
+    }
+    
+}
